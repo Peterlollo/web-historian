38e77cd04986222d848ba6c1246a6d5e41e4e067
peterlollo <peteralollo@gmail.com>
2016-02-04 12:12:00 -0800

    Create worker program

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,14 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var archive = require('../helpers/archive-helpers');
+
+archive.downloadSiteFile( (data) => {
+  var current = {};
+  var siteArray = [];
+  if ( data ) current = JSON.parse( data );
+  for(var site in current) {
+    if( current[site] === false) siteArray.push(site); 
+  } 
+  archive.downloadUrls(siteArray);
+});
+
