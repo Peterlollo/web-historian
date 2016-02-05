23f66ebb4230f3880ab96d4d5bc3d5a2ff4e3cec
peterlollo <peteralollo@gmail.com>
2016-02-04 19:27:34 -0800

    Fix unresolved DNS bug

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,7 +1,9 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
-var archive = require('../helpers/archive-helpers');
+var archive = require('/Volumes/student/webHistorian/helpers/archive-helpers');
 var fs = require( 'fs' );
+
+console.log( 'we so CRON right now');
 
 archive.downloadSiteFile( (data) => {
   var current = {};
